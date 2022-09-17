d82b32372339318e8778fdfc30025cd7a7850c6e
mathewNZ <41562099+mathewNZ@users.noreply.github.com>
2022-09-17 14:42:41 +1200

    Update Zenas_SL_App.py

--- Zenas_SL_App.py
+++ Zenas_SL_App.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('Zenas App')
 
+import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(),CURRENT_REGION()")
